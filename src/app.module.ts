8b46f14693895c822a181468cc37a4fbd4462531
Darab Ahmed <darabahmed37@gmail.com>
2023-02-22 20:08:46 +0500

    setting up configuration files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,24 @@ import { UserModule } from 'user/user.module';
 import { AuthModule } from 'auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JWTAuthGuard } from 'auth/jwt-guard';
-import { CONFIGURATIONS } from 'custom/configurations';
+import { databaseConfig } from 'custom/configurations';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
     UserModule,
     AuthModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: CONFIGURATIONS.DB_HOST,
-      port: CONFIGURATIONS.DB_PORT,
-      password: CONFIGURATIONS.DB_PASSWORD,
-      username: CONFIGURATIONS.DB_USERNAME,
-      database: CONFIGURATIONS.DB_NAME,
+      host: databaseConfig().DB_HOST,
+      port: databaseConfig().DB_PORT,
+      password: databaseConfig().DB_PASSWORD,
+      username: databaseConfig().DB_USERNAME,
+      database: databaseConfig().DB_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
