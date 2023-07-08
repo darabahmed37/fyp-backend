1ed2c6981a92d1586a9eb60e5f190ab3ac81a61a
Darab Ahmed <darabahmed37@gmail.com>
2023-07-08 18:50:44 +0500

    Added validation

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { RoleService } from 'role/role.service';
+import {Public} from "utils/custom.decorator";
 
 @Controller('role')
 export class RoleController {
   constructor(private roleService: RoleService) {}
 
   @Get()
+  @Public()
   async getAllRoles(): Promise<string[]> {
     return this.roleService.getAllRole();
   }
